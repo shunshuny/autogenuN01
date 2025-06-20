e033c2e271f50f3b390f88feb88bafe3098d9dec
shunshuny <shun020926@ezweb.ne.jp>
2025-06-20 16:31:08 +0900

    adjust weight

--- tank01.py
+++ tank01.py
@@ -21,16 +21,15 @@ settings.max_iter = 0
 settings.opterr_tol = 1e-06
 settings.verbose_level = 1 # print opt error
 
-horizon = cgmres.common.Horizon(Tf=1.0, alpha=0.5)
+horizon = cgmres.common.Horizon(Tf=1.0, alpha=0.0)
 
 
 # 重み係数の変更など
-# R_u = 20; R_u2 = 40; R_u3 = 80
-# Q_p_val = 1; Q_p_val2 = 1; Q_p_val3 = 1
-# ocpM.Q_s = ocpL.Q_s = ocpS.Q_s = 0.5; ocpM.Q_s2 = ocpL.Q_s2 = ocpS.Q_s2 = 0.5
-# ocpM.Q_s3 = ocpL.Q_s3 = ocpS.Q_s3 = 0.5
-# ocpM.ALPHA = ocpL.ALPHA = ocpS.ALPHA = 1; ocpM.qdash = ocpL.qdash = ocpS.qdash= 1
-# ocpM.sigma = ocpL.sigma = ocpS.sigma= 5
+ocp_nominal.Q1 = 1.0; ocp_nominal.Q2 = 100; ocp_nominal.Q3 = 10
+ocp_nominal.r1 = 0.2; ocp_nominal.r2 = 0.2; ocp_nominal.r3 = 0.2; ocp_nominal.r0 = 1; ocp_nominal.r4 = 100
+ocp_nominal.sf1 = 5; ocp_nominal.sf2 = 5; ocp_nominal.sf3 = 5
+
+ocp_nominal.fb_eps = np.array([0.01])
 
 player = 3
 
@@ -39,24 +38,25 @@ t0 = 0.0
 x_o = np.array([5.0, 3.0, 4.0])
 x0_nominal = np.concatenate([x_o, x_o, x_o])
 
-# Initialize solution using zero horizon OCP solution (by racer_M)
+# Initialize solution using zero horizon OCP solution
 initializer_nominal = cgmres.tank.ZeroHorizonOCPSolver(ocp_nominal, settings)
-uc0_nominal = np.array([1.0, 0.5, 0.5, 0.5])
+uc0_nominal = np.array([1.0, 0.5, 0.5, 0.5, 0.01])
 initializer_nominal.set_uc(uc0_nominal)
 initializer_nominal.solve(t0, x0_nominal)
 
-# Create MPC solver and set the initial solution (by racer_M)
-mpc_nominal = cgmres.tank.MultipleShootingCGMRESSolver(ocp_nominal, horizon,settings)
+# Create MPC solver and set the initial solution
+# mpc_nominal = cgmres.tank.MultipleShootingCGMRESSolver(ocp_nominal, horizon,settings)
+mpc_nominal = cgmres.tank.SingleShootingCGMRESSolver(ocp_nominal, horizon,settings)
 mpc_nominal.set_uc(initializer_nominal.ucopt)
-mpc_nominal.init_x_lmd(t0, x0_nominal)
-mpc_nominal.init_dummy_mu()
+# mpc_nominal.init_x_lmd(t0, x0_nominal)
+# mpc_nominal.init_dummy_mu()
 
 
 # logファイル、ディレクトリ生成
 logger = Logger(log_dir='logs/tank01', log_name='tank01')
 
 # simple simulation with forward Euler ?? RK4 :Computes the next state by the 4th-order Runge-Kutta method. 
-tsim = 500
+tsim = 1000
 sampling_time = settings.sampling_time
 t = t0
 x = x0_nominal.copy()
@@ -67,51 +67,16 @@ x = x0_nominal.copy()
 num_steps = int(tsim / sampling_time)
 
 for i in range(num_steps):
-    u_nominal = mpc_nominal.uopt[0]
-    # buffer = []
-    # for j in range(len(mpcM.xopt)):
-    #     position = np.concatenate([mpcM.xopt[j][:3], mpcM.xopt[j][15:18], mpcM.xopt[j][30:33]])
-    #     rounded = [round(k,3) for k in position] #小数点以下三桁に丸め
-    #     buffer.extend(rounded) #ホライゾン上のｘを一つのリストに結合　サイズ：9*Nグリッド
-    # M_horizon.append(buffer)
-    # buffer = []
-    # for j in range(len(mpcL.xopt)):
-    #     position = np.concatenate([mpcL.xopt[j][:3], mpcL.xopt[j][15:18], mpcL.xopt[j][30:33]])
-    #     rounded = [round(k,3) for k in position] #小数点以下三桁に丸め
-    #     buffer.extend(rounded) #ホライゾン上のｘを一つのリストに結合　サイズ：9*Nグリッド
-    # L_horizon.append(buffer)
-    # buffer = []
-    # for j in range(len(mpcS.xopt)):
-    #     position = np.concatenate([mpcS.xopt[j][:3], mpcS.xopt[j][15:18], mpcS.xopt[j][30:33]])
-    #     rounded = [round(k,3) for k in position] #小数点以下三桁に丸め
-    #     buffer.extend(rounded) #ホライゾン上のｘを一つのリストに結合　サイズ：9*Nグリッド
-    # S_horizon.append(buffer)
+    u_nominal = mpc_nominal.uopt[0] # 系列の最初を入力とする
+    x_nominal = RK4(ocp_nominal, t, sampling_time, x, u_nominal) # xを次の状態へ更新
+    mpc_nominal.update(t, x_nominal)
 
-    # x_o, x_other =np.split(x, [3]) 
-    x_nominal = RK4(ocp_nominal, t, sampling_time, x, u_nominal)
-    # xL = RK4(ocpL, t, sampling_time, x_o, uL)
-    # xS = RK4(ocpS, t, sampling_time, x_o, uS)
-    
-    # xM_p1, xM_p2, xM_p3, xM_other = np.split(xM,[15,30,45])
-    # xL_p1, xL_p2, xL_p3 = np.split(xL,[15,30])
-    # xS_p1, xS_p2, xS_p3 = np.split(xS,[15,30])
-    # x_o = np.r_[xS_p1, xM_p2, xL_p3]
-    # x = np.concatenate([x_o, x_o, x_o])
-    x = x_nominal.copy()
-    
-    # uM_p1, uM_p2, uM_p3 = np.split(uM,[4,8])
-    # uL_p1, uL_p2, uL_p3 = np.split(uL,[4,8])
-    # uS_p1, uS_p2, uS_p3 = np.split(uS,[4,8])
-    u = u_nominal.copy()
-    
-    
-    opt_error = np.r_[mpc_nominal.opt_error()]
-    
-    logger.save(t, x, u, opt_error)
-    
-    t = t0 + (i + 1) * sampling_time
+    logger.save(t, x, u_nominal, mpc_nominal.opt_error())
 
-    mpc_nominal.update(t, x)
+    t = t + sampling_time
+    x = x_nominal
+    x_o = x[:3]  # 3つの水位を抽出
+
     print('t: ', t, ', x: ', x_o)
     # mpcL.update(t, x_o)
     # print('t: ', t, ', x: ', x_o)
@@ -120,21 +85,6 @@ for i in range(num_steps):
     # #print('t: ', t)
     
 logger.close
-# with open('logs/drone_p3MNP/drone_p3MNP_horizon_M.csv', 'w', newline='') as f:
-#     writer = csv.writer(f, delimiter=' ')
-    
-#     for row in M_horizon:
-#         writer.writerow(row)
-# with open('logs/drone_p3MNP/drone_p3MNP_horizon_L.csv', 'w', newline='') as f:
-#     writer = csv.writer(f, delimiter=' ')
-#     for row in L_horizon:
-#         writer.writerow(row)
-# with open('logs/drone_p3MNP/drone_p3MNP_horizon_S.csv', 'w', newline='') as f:
-#     writer = csv.writer(f, delimiter=' ')
-#     for row in S_horizon:
-#         writer.writerow(row)
-
-
 
 print("\n======================= MPC used in this simulation: =======================")
 print(mpc_nominal)
