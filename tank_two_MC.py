ad2d5a3f659de0f09030c14ba468f2b291e34336
shunshuny <shun020926@ezweb.ne.jp>
2025-06-25 22:45:50 +0900

    for_MC_method

---
+++ tank_two_MC.py
@@ -0,0 +1,124 @@
+import cgmres.tank_two
+import cgmres.common
+import numpy as np
+from autogenu import RK4, Logger, Plotter, forward_euler
+import sympy
+from sympy import sin, cos, tan, exp, log, sinh, cosh, tanh, diff, sqrt, Matrix, integrate
+import csv
+import time
+
+def forward_euler_MC(t, dt, x: np.ndarray, u: np.ndarray):
+    dx = np.zeros(np.size(x))
+    dx[0] = (u[0] - u[1]*tanh(a*(x[0]-x[1]))*sqrt(2*g*(x[0]-x[1])*tanh(b*(x[0]-x[1]))) -q1*(1 + alpha*theta1))/S1
+    dx[1] = (- u[1]*tanh(a*(x[1]-x[0]))*sqrt(2*g*(x[1]-x[0])*tanh(b*(x[1]-x[0]))) -q2*(1 + alpha*theta2))/S2
+    dx[2] = dx[0]
+    dx[3] = dx[1]
+    return x + dx * dt
+
+start = time.time() #コードの開始時刻
+
+ocp_nominal = cgmres.tank_two.OCP()
+
+settings = cgmres.common.SolverSettings()
+settings.sampling_time = 0.01
+settings.zeta = 100
+settings.finite_difference_epsilon = 1e-08
+settings.max_iter = 0
+settings.opterr_tol = 1e-06
+settings.verbose_level = 0 # print opt error
+
+horizon = cgmres.common.Horizon(Tf=1.5, alpha=0.5)
+
+
+# 重み係数の変更など
+ocp_nominal.Q1 = 0
+ocp_nominal.Q2 = 10
+ocp_nominal.r4 = 1
+ocp_nominal.sf1 = 5
+ocp_nominal.sf2 = 5
+ocp_nominal.x1_min = 4.5
+ocp_nominal.x2_min = 0.0
+
+# モデルのパラメータの設定
+a = 0.5
+b = 0.2
+g = 9.78
+S1 = 25.0
+S2 = 25.0
+q1 = 1.5
+q2 = 0.5
+alpha = 0.1
+theta1 = np.random.uniform(-1, 1)
+theta2 = np.random.uniform(-1, 1)
+
+
+# ocp_nominal.fb_eps = np.array([0.01])
+
+player = 2
+
+# 初期状態の設定
+t0 = 0.0
+x_o = np.array([5.0, 3.0])
+x0_nominal = np.concatenate([x_o, x_o])
+
+# Initialize solution using zero horizon OCP solution
+initializer_nominal = cgmres.tank_two.ZeroHorizonOCPSolver(ocp_nominal, settings)
+uc0_nominal = np.array([1.0, 0.5, 0.01, 0.01])  # Initial guess for the control inputs
+initializer_nominal.set_uc(uc0_nominal)
+initializer_nominal.solve(t0, x0_nominal)
+
+
+# Create MPC solver and set the initial solution
+# mpc_nominal = cgmres.tank_two.MultipleShootingCGMRESSolver(ocp_nominal, horizon,settings)
+mpc_nominal = cgmres.tank_two.SingleShootingCGMRESSolver(ocp_nominal, horizon, settings)
+mpc_nominal.set_uc(initializer_nominal.ucopt)
+# mpc_nominal.init_x_lmd(t0, x0_nominal)
+mpc_nominal.init_dummy_mu()
+
+
+# logファイル、ディレクトリ生成
+logger = Logger(log_dir='logs/tank_two_MC', log_name='tank_two_MC')
+
+# simple simulation with forward Euler ?? RK4 :Computes the next state by the 4th-order Runge-Kutta method. 
+tsim = 500
+sampling_time = settings.sampling_time
+t = t0
+x = x0_nominal.copy()
+
+# ホライゾン上のｘの保存
+# nominal_horizon = []
+
+num_steps = int(tsim / sampling_time)
+
+
+for i in range(num_steps):
+    
+    u_nominal = mpc_nominal.uopt[0] # 系列の最初を入力とする
+    x_nominal = forward_euler_MC(t, sampling_time, x, u_nominal) # xを次の状態へ更新
+    # x_nominal = RK4(ocp_nominal, t, sampling_time, x, u_nominal)
+    mpc_nominal.update(t, x_nominal)
+
+    x = x_nominal
+    x_o = x[:2]  # 2つの水位を抽出
+    logger.save(t, x_o, u_nominal, mpc_nominal.opt_error())
+
+    t = t + sampling_time
+
+    print('t: ', t, ', x: ', x_o)
+
+logger.close
+
+print("\n======================= MPC used in this simulation: =======================")
+print(mpc_nominal)
+
+end = time.time() #コードの終了時刻
+time_diff = end - start
+minute, second = time_diff // 60, time_diff % 60 # 計算時間を時間と秒に分ける
+print('All Calculate Time = ', minute, 'minute', second,'second') 
+
+# time.sleep(5)
+
+# plotter = Plotter(log_dir='logs/tank01', log_name='tank01', player = player)
+# plotter.show()
+# plotter.save()
+
